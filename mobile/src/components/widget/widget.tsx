3e8699ba52cc8eae6185f83d6f7774128b13da82
Wimull <aninhasilva@tutanota.com>
2022-05-05 11:58:43 -0300

    Loaded fonts and added assets

---
+++ mobile/src/components/widget/widget.tsx
@@ -0,0 +1,7 @@
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+import { styles } from "./styles";
+
+export function Widget() {
+	return <Text style={styles.container}>Hello World!!!</Text>;
+}
